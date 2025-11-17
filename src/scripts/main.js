f8c03a05533e1d9afe88eaf0c21cfff1bb9e6587
Marina Yakubovskaya <maribars93@yandex.ru>
2025-11-18 00:58:14 +0300

    feat: add favicon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,14 @@
 import '../styles/style.scss';
 import { renderStartScreen } from './render-start-screen';
 
+const addFavicon = (url) => {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/x-icon';
+  link.href = url;
+  document.head.appendChild(link);
+};
+
+addFavicon('./favicon.ico');
+
 renderStartScreen();
