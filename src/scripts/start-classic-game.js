5e5044cda4381f72cbac3863d0f79a5f7183bcff
Marina Yakubovskaya <maribars93@yandex.ru>
2025-11-17 22:14:28 +0300

    feat: add control and  assist buttons, settings button and add style for elements

--- src/scripts/start-classic-game.js
+++ src/scripts/start-classic-game.js
@@ -47,12 +47,75 @@ const startClassicGame = () => {
   scoreContainer.appendChild(maxLabel);
   scoreContainer.appendChild(maxValue);
 
+  const controlButtonsContainer = document.createElement('div');
+  controlButtonsContainer.classList.add('game__control-buttons');
+
+  const continueButton = document.createElement('button');
+  continueButton.classList.add('control-button', 'button');
+  continueButton.textContent = 'Continue';
+  continueButton.disabled = true;
+
+  const resetButton = document.createElement('button');
+  resetButton.classList.add('control-button', 'button');
+  resetButton.textContent = 'Reset';
+
+  const saveButton = document.createElement('button');
+  saveButton.classList.add('control-button', 'button');
+  saveButton.textContent = 'Save Game';
+
+  controlButtonsContainer.appendChild(continueButton);
+  controlButtonsContainer.appendChild(resetButton);
+  controlButtonsContainer.appendChild(saveButton);
+
+  const settingsButton = document.createElement('button');
+  settingsButton.classList.add('settings-button', 'button');
+  settingsButton.textContent = 'Settings';
+
+  const assistButtonsContainer = document.createElement('div');
+  assistButtonsContainer.classList.add('game__assist-buttons');
+
+  const hintsButton = document.createElement('button');
+  hintsButton.classList.add('assist-button', 'button');
+  hintsButton.textContent = 'Hints';
+
+  const revertButton = document.createElement('button');
+  revertButton.classList.add('assist-button', 'button');
+  revertButton.textContent = 'Revert';
+  revertButton.disabled = true;
+
+  const addNumbersButton = document.createElement('button');
+  addNumbersButton.classList.add('assist-button', 'button');
+  addNumbersButton.textContent = 'Add Numbers';
+
+  const shuffleButton = document.createElement('button');
+  shuffleButton.classList.add('assist-button', 'button');
+  shuffleButton.textContent = 'Shuffle';
+
+  const eraserButton = document.createElement('button');
+  eraserButton.classList.add('assist-button', 'button');
+  eraserButton.textContent = 'Eraser';
+
+  assistButtonsContainer.appendChild(revertButton);
+  assistButtonsContainer.appendChild(hintsButton);
+  assistButtonsContainer.appendChild(addNumbersButton);
+  assistButtonsContainer.appendChild(shuffleButton);
+  assistButtonsContainer.appendChild(eraserButton);
+
+  const gameContentWrapper = document.createElement('div');
+  gameContentWrapper.classList.add('game__content-wrapper');
+
   const gameGridContainer = document.createElement('div');
   gameGridContainer.classList.add('game__grid-container');
 
+  gameContentWrapper.appendChild(controlButtonsContainer);
+  gameContentWrapper.appendChild(gameGridContainer);
+  gameContentWrapper.appendChild(assistButtonsContainer);
+
   gameContainer.appendChild(gameTitle);
   gameContainer.appendChild(scoreContainer);
-  gameContainer.appendChild(gameGridContainer);
+  gameContainer.appendChild(settingsButton);
+  gameContainer.appendChild(gameContentWrapper);
+
   document.body.appendChild(gameContainer);
 
   updateScoreDisplay();
